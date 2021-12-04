970223ee9806317c572d8cbacaaf7a7dbea1c0a8
Scott Reoch <scottreoch10@hotmail.co.uk>
2021-12-04 14:33:40 +0000

    added list of members on members page and also a form to create new member

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -9,3 +9,13 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", members=members)
+
+@members_blueprint.route("/members/new", methods=["POST"])
+def add_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    age = request.form['age']
+    membership_type = request.form['membership_type']
+    member = Member(first_name, last_name, age, membership_type)
+    member_repository.save(member)
+    return redirect('/members')
