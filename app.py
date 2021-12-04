43a9013203fc13ab687da03a36abc77ddeb4e363
Scott Reoch <scottreoch10@hotmail.co.uk>
2021-12-04 13:22:35 +0000

    created frontend folder structure, started on members controller and template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 # from controllers.bookings_controller import bookings_blueprint
-# from controllers.members_controller import members_blueprint
+from controllers.members_controller import members_blueprint
 # from controllers.classes_controller import classes_blueprint
 
 app = Flask(__name__)
 
 # app.register_blueprint(bookings_blueprint)
-# app.register_blueprint(members_blueprint)
+app.register_blueprint(members_blueprint)
 # app.register_blueprint(classes_blueprint)
 
 @app.route('/')
